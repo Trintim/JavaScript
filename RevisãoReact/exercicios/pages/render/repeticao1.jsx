c8eb5d46ce63df396ed79a172cb7620cf6a6f9bf
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2022-03-30 22:32:32 -0300

    commit

---
+++ RevisãoReact/exercicios/pages/render/repeticao1.jsx
@@ -0,0 +1,17 @@
+export default function repeticao1(){
+    const listaAprovados = [
+        'Victor',
+        'Taina',
+        'Iasmin',
+        'Gilson',
+        'Fabin',
+        'Levi',
+    ]
+    return (
+        <ul>
+            <li>Elemento #01</li>
+            <li>Elemento #02</li>
+            <li>Elemento #03</li>
+        </ul>
+    )
+}
